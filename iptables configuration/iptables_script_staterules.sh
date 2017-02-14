561da7eba2c855cb1be1c5e5a4be49f807983b89
Alejandro <alejandro@alsnet.es>
2017-02-14 17:49:08 +0000

    Update iptables_script_staterules.sh

--- iptables configuration/iptables_script_staterules.sh
+++ iptables configuration/iptables_script_staterules.sh
@@ -3,34 +3,28 @@ iptables -F
 
 # - Reglas por defecto
 iptables -P INPUT DROP
-iptables -P OUTPUT ACCEPT
+iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
 # - Acepto establecidas y relacionadas
-iptables -A FORWARD -m state --state ESTABLISHED, RELATED -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# - Permitir tráfico loopback
-iptables -A INPUT -i lo -j ACCEPT
+# - OUTPUT
+iptables -A OUTPUT -o lo -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p icmp -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443,8443 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
 
-# - Tráfico saliente ICMP
-iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -p icmp --icmp-type echo-reply -j ACCEPT
-iptables -A FORWARD -i eth1 -o eth0 -p icmp --icmp-type echo-request -j ACCEPT 
+# - INPUT
+iptables -A INPUT -i lo -m state --state NEW -j ACCEPT
+iptables -A INPUT -i eth1 -s 10.110.7.0/24 -p icmp -m state --state NEW -j ACCEPT
+iptables -A INPUT -i eth1 -s 10.110.7.0/24 -p tcp --dport 22 -m state --state NEW -j ACCEPT
 
-# - Responder ICMP red interna
-iptables -A INPUT -p icmp -s 10.110.7.0/24 -j ACCEPT
-
-# - Dar acceso HTTP, HTTPS, DNS y Campus Virtual
-# Pasarela
-iptables -A INPUT -p tcp -m multiport --sport 53,80,443,8443 -j ACCEPT
-# Workstation
-iptables -A FORWARD -p tcp -m multiport --dport 53,80,443,8443 -m --state NEW -j ACCEPT
-
-# - Aceptar SSH/SFTP en la pasarela desde red interna
-iptables -A INPUT -p tcp --dport 22 -d 10.110.7.0/24 -m --state NEW -j ACCEPT
-
-# - Permitir SSH a Neptuno: neptuno.redes.dis.ulpgc.es
-# Workstation
-iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -m --state NEW -j ACCEPT
-iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -j LOG
-iptables -A FORWARD -p tcp --sport 22 -s 10.110.1.24 -j LOG
+# - FORWARD
+iptables -A FORWARD -p icmp -i eth1 -o eth0 -s 10.110.7.0/24 -m state --state NEW -j ACCEPT
+iptables -A FORWARD -p tcp -m multiport --dports 80,443,8443 -m state --state NEW -j ACCEPT
+iptables -A FORWARD -p udp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -m state --state NEW -j LOG --log-prefix "SSH in da house:"
+iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -m state --state NEW -j ACCEPT
