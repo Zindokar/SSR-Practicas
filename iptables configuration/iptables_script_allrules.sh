c90656e94be7a48989aee102412ab2471ef0fff6
Alejandro <alejandro@alsnet.es>
2017-02-14 19:57:37 +0000

    Update iptables
    Final release, all working

--- iptables configuration/iptables_script_allrules.sh
+++ iptables configuration/iptables_script_allrules.sh
@@ -41,3 +41,4 @@ iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -j LOG --log-prefix "SSH in
 iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -j ACCEPT
 iptables -A FORWARD -p tcp --sport 22 -s 10.110.1.24 -j LOG --log-prefix "SSH in da house:"
 iptables -A FORWARD -p tcp --sport 22 -s 10.110.1.24 -j ACCEPT
+
--- iptables configuration/iptables_script_natrules.sh
+++ iptables configuration/iptables_script_natrules.sh
@@ -1,5 +1,6 @@
-# - Borro la tabla
+# - Borro las tablas
 iptables -F
+iptables -t nat -F
 
 # - Reglas por defecto
 iptables -P INPUT DROP
@@ -19,13 +20,19 @@ iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
 
 # - INPUT
 iptables -A INPUT -i lo -m state --state NEW -j ACCEPT
-iptables -A INPUT -i eth1 -s 10.110.7.0/24 -p icmp -m state --state NEW -j ACCEPT
-iptables -A INPUT -i eth1 -s 10.110.7.0/24 -p tcp --dport 22 -m state --state NEW -j ACCEPT
+iptables -A INPUT -i eth1 -s 192.168.120.0/24 -p icmp -m state --state NEW -j ACCEPT
+iptables -A INPUT -i eth1 -s 192.168.120.0/24 -p tcp --dport 22 -m state --state NEW -j ACCEPT
 
 # - FORWARD
-iptables -A FORWARD -p icmp -i eth1 -o eth0 -s 10.110.7.0/24 -m state --state NEW -j ACCEPT
+iptables -A FORWARD -p icmp -i eth1 -o eth0 -s 192.168.120.0/24 -m state --state NEW -j ACCEPT
 iptables -A FORWARD -p tcp -m multiport --dports 80,443,8443 -m state --state NEW -j ACCEPT
 iptables -A FORWARD -p udp --dport 53 -m state --state NEW -j ACCEPT
 iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -m state --state NEW -j LOG --log-prefix "SSH in da house:"
 iptables -A FORWARD -p tcp --dport 22 -d 10.110.1.24 -m state --state NEW -j ACCEPT
+
+# - Postrouting, redirijo workstation a internet
+iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to 10.110.1.7
+
+# - Prerouting, redirijo internet a workstation puerto 80
+iptables -t nat -A PREROUTING -p tcp -d 10.110.1.7 --dport 80 -j DNAT --to 192.168.120.2:80
 
